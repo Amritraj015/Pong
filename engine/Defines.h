7491157807b7154287bfdc8a7e1a7b81d5ac8320
amritrjakacabu015@gmail.com <amritrjakacabu015@gmail.com>
2024-04-09 22:01:55 -0400

    Defining the initial Platform interface.

---
+++ engine/Defines.h
@@ -0,0 +1,81 @@
+#pragma once
+
+// Unsigned int types.
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef unsigned long long u64;
+
+// Signed int types.
+typedef signed char i8;
+typedef signed short i16;
+typedef signed int i32;
+typedef signed long long i64;
+
+// Floating point types
+typedef float f32;
+typedef double f64;
+
+// Properly define static assertions.
+#if defined(__clang__) || defined(__gcc__)
+#define STATIC_ASSERT _Static_assert
+#else
+#define STATIC_ASSERT static_assert
+#endif
+
+// Ensure all types are of the correct size.
+STATIC_ASSERT(sizeof(u8) == 1, "Expected u8 to be 1 byte.");
+STATIC_ASSERT(sizeof(u16) == 2, "Expected u16 to be 2 bytes.");
+STATIC_ASSERT(sizeof(u32) == 4, "Expected u32 to be 4 bytes.");
+STATIC_ASSERT(sizeof(u64) == 8, "Expected u64 to be 8 bytes.");
+
+STATIC_ASSERT(sizeof(i8) == 1, "Expected i8 to be 1 byte.");
+STATIC_ASSERT(sizeof(i16) == 2, "Expected i16 to be 2 bytes.");
+STATIC_ASSERT(sizeof(i32) == 4, "Expected i32 to be 4 bytes.");
+STATIC_ASSERT(sizeof(i64) == 8, "Expected i64 to be 8 bytes.");
+
+STATIC_ASSERT(sizeof(f32) == 4, "Expected f32 to be 4 bytes.");
+STATIC_ASSERT(sizeof(f64) == 8, "Expected f64 to be 8 bytes.");
+
+// Platform detection
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
+
+#define PLATFORM_WINDOWS 1
+#include <windows.h>
+#include <windowsx.h> // param input extraction
+
+#elif defined(__linux__) || defined(__gnu_linux__)
+
+// Linux OS
+#define PLATFORM_LINUX 1
+
+// For Vulkan surface creation
+#define VK_USE_PLATFORM_XCB_KHR
+// #define VK_USE_PLATFORM_WAYLAND_KHR
+
+#include <xcb/xcb.h>
+#include <X11/keysym.h>
+#include <X11/XKBlib.h> // sudo apt-get install libx11-dev
+#include <X11/Xlib.h>
+#include <X11/Xlib-xcb.h> // sudo apt-get install libxkbcommon-x11-dev
+#include <sys/time.h>
+
+#else
+#error "Unknown platform!"
+#endif
+
+#include <cstring>
+#include <cstdarg>
+
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <algorithm>
+#include <functional>
+
+#include <string>
+#include <sstream>
+#include <vector>
+#include <array>
+#include <unordered_map>
+#include <unordered_set>
---
+++ engine/core/StatusCode.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace PongEngine
+{
+    /// @brief This `enum` defines status codes to represent various operation results.
+    enum class StatusCode
+    {
+        Successful = 0,           // Operation Successful.
+        WindowCreationFailed = 1, // Represents an error that could occur on window creation.
+        WindowCloseFailed = 2,    // Represents an error that could occur while closing the window.
+    };
+}
